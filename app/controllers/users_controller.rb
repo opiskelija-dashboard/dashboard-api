9359f5b910eb5bf3962b2884198759e1f9034210
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-01 17:02:31 +0300

    Uncomment skip_before_action :authenticate_request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   # Uncomment this if you want to test the application without auth tokens.
-  #skip_before_action :authenticate_request
+  skip_before_action :authenticate_request
 
   def demo
     render json: User.first

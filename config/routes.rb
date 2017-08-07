30288b89ffdb77a8f9fca307fcb14a1ba5485447
Michael Macphee <macphee@cs.helsinki.fi>
2017-08-07 18:55:47 +0300

    Preliminary LeaderboardsController
    Check config/routes.rb for usage and addresses

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   get '/cumulative-points', to: 'cumulative_points#cumulative_point_current'
   get '/skill-percentages', to: 'mastery_percentages#skill_percentage_current'
   post '/new-dash-session', to: 'tokens#newtoken'
+
+  get '/leaderboard/course/:course_id', to: 'leaderboards#get_range'
+  get '/leaderboard/course/:course_id/all', to: 'leaderboards#get_all'
+  get '/leaderboard/course/:course_id/whereis/:user_id', to: 'leaderboards#find_user'
+  get '/leaderboard/course/:course_id/update', to: 'leaderboards#update_points'
+
+
 end

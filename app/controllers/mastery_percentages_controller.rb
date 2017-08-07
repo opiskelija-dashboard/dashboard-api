08ce7ce80bbb818ba742f86964ab72e3d5bc688f
Michael Macphee <oatcookies@users.noreply.github.com>
2017-08-07 12:44:54 +0300

    Whitespace fix

--- app/controllers/mastery_percentages_controller.rb
+++ app/controllers/mastery_percentages_controller.rb
@@ -1,12 +1,12 @@
 class MasteryPercentagesController < ApplicationController
   before_action :mastery_percentage
-  
+
   def skill_percentage_current
     render json: @mastery_percentage
   end
-  
+
   private
-  
+
   def mastery_percentage
     @mastery_percentage = MasteryPercentage.new
   end

15110a79ef425b2ca33463831e37d1d815bdda17
Michael Macphee <oatcookies@users.noreply.github.com>
2017-08-07 12:44:17 +0300

    Whitespace fix

--- app/controllers/cumulative_points_controller.rb
+++ app/controllers/cumulative_points_controller.rb
@@ -1,12 +1,12 @@
 class CumulativePointsController < ApplicationController
-  before_action :set_cumulative_point  
-  
+  before_action :set_cumulative_point
+
   def cumulative_point_current
     render json: @cumulative_point
   end
-  
+
   private
-  
+
   def set_cumulative_point
     @cumulative_point = CumulativePoint.new
   end

093c149d861cdc1b01b37d9bd89b3bf1a7f7f673
Jussi Maristo <maot@cs.helsinki.fi>
2017-08-09 14:14:48 +0300

    started mastery percentage spec

--- spec/controllers/mastery_percentage_controller_spec.rb
+++ spec/controllers/mastery_percentage_controller_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require 'jwt'
 
-describe CumulativePointsController do
+describe MasteryPercentageController do
   context 'when correct authorization credentials are given' do
 
     before do
@@ -12,23 +12,21 @@ describe CumulativePointsController do
       "tmctok" => "token",
       "exp" => (Time.now + 86400).to_i
       }
-       
+
       JWT_HASH_ALGO = 'HS256'
       token = JWT.encode(token_payload, jwt_secret, JWT_HASH_ALGO)
       header "Authorization", "Bearer #{token}"
 
-      get '/cumulative-points'
+      get '/skill-percentages'
     end
 
     it 'responds with a 200 status' do
       expect(last_response.status).to eq 200
     end
 
-#    it 'returns days, points and average' do
-#      expect(last_response.body).to have_content 'days:'
-#      expect(last_response.body).to have_content 'points:'
-#      expect(last_response.body).to have_content 'average:'
-#    end
+   it 'returns days, points and average' do
+     
+   end
   end
 
 end

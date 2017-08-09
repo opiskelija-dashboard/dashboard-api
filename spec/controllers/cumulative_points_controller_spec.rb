74824193d04c2e89ec4dd73ade270ee261dbed3f
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-09 14:52:33 +0300

    add spec helper for jwt token

--- spec/controllers/cumulative_points_controller_spec.rb
+++ spec/controllers/cumulative_points_controller_spec.rb
@@ -5,15 +5,7 @@ describe CumulativePointsController do
   context 'when correct authorization credentials are given' do
     
     before do
-      jwt_secret = Rails.configuration.jwt_secret
-      token_payload = {
-      "tmcusr" => "user",
-      "tmctok" => "token",
-      "exp" => (Time.now + 86400).to_i
-      }
-      JWT_HASH_ALGO = 'HS256'
-      token = JWT.encode(token_payload, jwt_secret, JWT_HASH_ALGO)
-      header "Authorization", "Bearer #{token}"
+      jwt_token
       
       get '/cumulative-points'
     end
@@ -28,5 +20,4 @@ describe CumulativePointsController do
       expect(json["average"]).not_to be_nil
     end
   end
-
 end

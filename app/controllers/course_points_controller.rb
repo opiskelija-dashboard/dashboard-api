5afcafc7177f37d1829250f345dc528d43318574
Michael Macphee <macphee@cs.helsinki.fi>
2017-08-15 16:19:41 +0300

    New route: /course-points/:course-id/update
    The idea is that there's a way to update the course points
    without having to explicitly get leaderboard or cumulative
    point data.

---
+++ app/controllers/course_points_controller.rb
@@ -0,0 +1,32 @@
+class CoursePointsController < ApplicationController
+
+  def initialize()
+    @point_source = Rails.configuration.points_store_class == 'MockPointsStore' ? MockPointsStore : PointsStore
+  end
+
+  # Update if necessary.
+  # GET /course-data/:course_id/update
+  def update
+    course_id = params[:course_id].to_s
+
+    if (@point_source.course_point_update_needed?(course_id) == false)
+      render json: { "data" => "Points of course " + course_id + " not updated because data isn't too old yet" }, status: 200 #ok
+      return
+    end
+
+    update_attempt = @point_source.update_course_points(course_id, @token)
+
+    if (update_attempt[:success])
+      course_points = @point_source.course_points(course_id)
+      render json: { "data" => "OK, updated points of course " + course_id }, status: 200
+    else
+      errors = update_attempt[:errors]
+      errors.push({
+        "title" => "Unable to update points of course " + course_id,
+        "detail" => "The update failed at the point source."
+      });
+      render json: { "errors" => errors }, status: 500 #server error
+    end
+  end
+
+end

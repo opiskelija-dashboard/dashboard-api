1f569ea6d0ac0f29fc83b91d909895744c715c3b
Juuha <juha.ritakoski@hotmail.com>
2017-08-16 14:17:40 +0300

    merge into master

--- app/models/badge_checker.rb
+++ app/models/badge_checker.rb
@@ -4,30 +4,69 @@
 class BadgeChecker
 
   # This is only temporary because we have an old model of CP.
-  def user_has_one_point(id)
-    CumulativePoint.new(Token.new).user_points[0].count >= 20
+  #def user_has_one_point(id)
+  #  CumulativePoint.new(Token.new).user_points[0].count >= 20
+  #end
+
+  #def user_is_cool(id)
+  #  true if id == 2
+  #end
+
+  #def lol(id)
+  #  true if id == 3
+  #end
+  def initialize(course_id, token)
+    @point_source = Rails.configuration.points_store_class == 'MockPointsStore' ? MockPointsStore : PointsStore
+
+    @course_id = course_id
+    @token = token
+
+    if (!@point_source.has_course_points?(@course_id))
+      Rails.logger.debug("PointsStore didn't have points of course " + @course_id.to_s + ", fetching...");
+      @point_source.update_course_points(@course_id, token)
+    end
   end
 
-  def user_is_cool(id)
-    true if id == 2
+  def achievement_predication_environment(user_id, all_points)
+    return binding()
   end
 
-  def lol(id)
-    true if id == 3
+  #returns a hash of unique user_ids in points
+  def get_user_ids(all_points)
+    user_ids = {}
+    all_points.each do |point|
+      user_ids[point["awarded_point"]["user_id"]] = 0
+    end
+    return user_ids
   end
 
   # Checks which badges a user (user_id) is worthy of.
-  def check(id)
-    badge_info = {}
-    Rails.configuration.badges.each do |badge|
-      badge_info[badge["badge"]["name"]] = true
-      badge["badge"]["criteria"].each do |criteria|
-        if !(BadgeChecker.new.send(criteria, id))
-          badge_info[badge["badge"]["name"]] = false
+  def check()
+    badges = get_badges
+    all_points = @point_source
+    points = @point_source.course_points(@course_id)
+    user_ids = get_user_ids(points)
+    user_ids.each do |user_id|
+      badges.each do |badge_definition|
+        binding = achievement_predication_environment(user_id, all_points)
+        
+        got_achievement = eval(badge_definition["criteria"], binding);
+
+        if (got_achievement)
+        # save to database here
+        puts("User " + user_id[0].to_s + " got achievement '" + badge_definition["name"] + "'");
         end
-      end
+      end  
     end
-    badge_info
+    return nil
   end
 
+  def get_badges
+    badges = YAML.load(File.open(Rails.root / 'config' / 'badges.yml'))
+    return badges
+  end
+
+  def get_point_source
+    @point_source
+  end
 end

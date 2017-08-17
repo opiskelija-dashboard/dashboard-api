9bcc3f2f53a2f7337e6abad41d31fb8d11be3beb
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-17 22:54:05 +0300

    checkstyle application_controller, heatmaps_controller, leaderboards_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,24 +6,24 @@ class ApplicationController < ActionController::API
   def authenticate_request
     token_string = unpack_token_from_header
 
-    if (token_string == false || token_string.nil?)
-      return false
-    end
+    return false if token_string == false || token_string.nil?
 
     token = Token.new_from_jwt_string(token_string)
-    if (token.valid?)
+    if token.valid?
       @token = token
       return true
     else
-      if (token.errors?)
-        render json: { # json:api format
-          "errors" => token.errors
+      if token.errors?
+        render json: {
+          errors: token.errors
         }, status: 401
       else
-        render json: { "errors" =>
+        render json:
+        {
+          errors:
           [{
-            "title" => "Unknown token error",
-            "detail" => "The JWT token you gave was invalid, but no more specific error is known."
+            title: 'Unknown token error',
+            detail: 'The JWT token you gave was invalid, but no more specific error is known.'
           }]
         }, status: 401
       end
@@ -37,26 +37,30 @@ class ApplicationController < ActionController::API
       regex = /^Bearer (.*)/
       encoded_token_string = header[regex, 1]
 
-      if (encoded_token_string.nil?)
-        render json: { "errors" =>
+      if encoded_token_string.nil?
+        render json:
+        {
+          errors:
           [{
-            "title" => "No JWT token in header",
-            "detail" => "The server's string processing facilities failed to see a token (prefixed by 'Bearer ') in the Authorization header."
+            title: 'No JWT token in header',
+            detail: "The server's string processing facilities failed to see
+                    a token (prefixed by 'Bearer ') in the Authorization header."
           }]
         }, status: 401
         return false
       end
+      encoded_token_string
 
-      return encoded_token_string
     else
-      render json: { "errors" =>
+      render json:
+      {
+        errors:
         [{
-          "title" => "Missing 'Authorization' header",
-          "detail" => "This resource requires a valid JSON Web Token signed by this server."
+          title: "Missing 'Authorization' header",
+          detail: 'This resource requires a valid JSON Web Token signed by this server.'
         }]
       }, status: 401
-      return false
+      false
     end
   end
-
 end

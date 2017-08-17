62407dd3158725e39547a774791d6bbb0e46a300
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-17 22:03:49 +0300

    checkstyle course_points_controller

--- app/controllers/cumulative_points_controller.rb
+++ app/controllers/cumulative_points_controller.rb
@@ -2,15 +2,18 @@ class CumulativePointsController < ApplicationController
   def show
     course_id = params['course_id']
     if course_id.nil?
-      render json: { errors:
-      [
-        {
-          title: 'Missing required course_id',
-          detail: 'Request address must be of the corm
-                  /cumulative-points/<course-id>, where <course-id>
-                  is the ID code of a TMC course.'
-        }
-      ]}, status: 400 # bad request
+      render json:
+      {
+        errors:
+        [
+          {
+            title: 'Missing required course_id',
+            detail: 'Request address must be of the corm
+                    /cumulative-points/<course-id>, where <course-id>
+                    is the ID code of a TMC course.'
+          }
+        ]
+      }, status: 400 # bad request
     end
     course_id = course_id.to_s
 

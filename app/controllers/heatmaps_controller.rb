738e179bac40bf51f409139a78a94753bb777878
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-08-20 17:17:21 +0300

    Refactor heatmap + controller

--- app/controllers/heatmaps_controller.rb
+++ app/controllers/heatmaps_controller.rb
@@ -8,7 +8,7 @@ class HeatmapsController < ApplicationController
     end
     course_id = course_id.to_s
     heatmap_for_everyone = Heatmap.new(course_id, @token)
-    render json: { 'data' => heatmap_for_everyone.get_everyones_points_average }
+    render json: { 'data' => heatmap_for_everyone.everyones_points_average }
   end
 
   # GET /heatmap/courses/:course_id/current-user
@@ -22,35 +22,28 @@ class HeatmapsController < ApplicationController
     heatmap_for_current_user = Heatmap.new(course_id, @token)
     render json: { 'data' => heatmap_for_current_user.current_user }
   end
+end
 
   private
 
-  def update_points_source_if_necessary(course_id, token)
-    return if @point_source.has_course_points?(course_id)
-    Rails.logger.debug("PointsStore didn't have points of
-     course #{course_id}, fetching...")
-    @point_source.update_course_points(course_id, token)
-  end
+def error_missing_required_course_id
+  { 'errors' => [
+    {
+      'title' => 'Missing required course_id',
+      'detail' => 'Request address must be of the corm
+       /heatmap/courses/<course_id>/all, where <course-id>
+        is the ID code of a TMC course.'
+    }
+  ] }
+end
 
-  def error_missing_required_course_id
-    { 'errors' => [
-      {
-        'title' => 'Missing required course_id',
-        'detail' => 'Request address must be of the corm
-         /heatmap/courses/<course_id>/all, where <course-id>
-          is the ID code of a TMC course.'
-      }
-    ] }
-  end
-
-  def error_missing_required_course_id_current_user
-    { 'errors' => [
-      {
-        'title' => 'Missing required course_id',
-        'detail' => 'Request address must be of the corm
-         /heatmap/courses/<course_id>/current-user, where <course-id>
-          is the ID code of a TMC course.'
-      }
-    ] }
-  end
+def error_missing_required_course_id_current_user
+  { 'errors' => [
+    {
+      'title' => 'Missing required course_id',
+      'detail' => 'Request address must be of the corm
+       /heatmap/courses/<course_id>/current-user, where <course-id>
+        is the ID code of a TMC course.'
+    }
+  ] }
 end
--- app/models/heatmap.rb
+++ app/models/heatmap.rb
@@ -1,46 +1,42 @@
 class Heatmap
-
-	def initialize(course_id, token)
-    @point_source = Rails.configuration.points_store_class == 'MockPointsStore' ? MockPointsStore : PointsStore
+  def initialize(course_id, token)
+    config = Rails.configuration.points_store_class
+    @point_source =
+      config == 'MockPointsStore' ? MockPointsStore : PointsStore
 
     @course_id = course_id
     @token = token
 
-    if (!@point_source.has_course_points?(@course_id))
-      Rails.logger.debug("PointsStore didn't have points of course " + @course_id + ", fetching...");
-      @point_source.update_course_points(@course_id, token)
-    end
-	end
+    return unless @point_source.has_course_points?(@course_id)
+    Rails.logger.debug("PointsStore didn't have points of course " +
+      @course_id + ', fetching...')
+    @point_source.update_course_points(@course_id, token)
+  end
 
-	def get_everyones_points_average(course_id, token)
+  def everyones_points_average
+    everyones_points = PointsHelper.all_course_points(@point_source, @course_id)
+    points_by_day = PointsHelper.daywise_points(everyones_points)
+    unique_users_by_week = PointsHelper.unique_users_by_week(everyones_points)
 
-		update_points_source_if_necessary(course_id, token)
-		everyones_points = PointsHelper.all_course_points(@point_source, course_id)
-		points_by_day = PointsHelper.daywise_points(everyones_points)
-		unique_users_per_week = PointsHelper.unique_users_per_week(everyones_points)
-
-		daily_average = Hash.new
-		points_by_day.each do |day, points|
-			week = Date.iso8601(day.to_s).strftime("%G-W%V")
-			users_this_week = unique_users_per_week[week].to_f
-			Rails.logger.debug("Day " + day.to_s + ", week " + week.to_s + ", points today " + points.to_s + ", users this week " + users_this_week.to_s)
-			if (users_this_week == 0)
-				avg = 0
-			else
-				avg = points / users_this_week
-			end
+    daily_average = {}
+    points_by_day.each do |day, points|
+      week = Date.iso8601(day.to_s).strftime('%G-W%V')
+      users_this_week = unique_users_by_week[week].to_f
+      Rails.logger.debug('Day ' + day.to_s + ', week ' + week.to_s + ', points today ' + points.to_s + ', users this week ' + users_this_week.to_s)
+      avg = if users_this_week == 0
+              0
+            else
+              points / users_this_week
+            end
 			daily_average[day] = avg
 		end
+    daily_average
+  end
 
-		render json: { "data" => daily_average }
-	end
-
-	# GET /heatmap/courses/:course_id/current-user
-	def get_current_user(course_id, token)
-		update_points_source_if_necessary(course_id, token)
-		current_users_points = PointsHelper.users_own_points(@point_source, course_id, token.user_id)
-		points_by_day = PointsHelper.daywise_points(current_users_points)
-		render json: { "data" => points_by_day }
-	end
-
+  # GET /heatmap/courses/:course_id/current-user
+  def current_user
+    current_users_points = PointsHelper.users_own_points(@point_source, @course_id, @token.user_id)
+    points_by_day = PointsHelper.daywise_points(current_users_points)
+    points_by_day
+  end
 end

51013657ac773740f465046ada46ec4c66280966
Juuha <juha.ritakoski@hotmail.com>
2017-08-23 12:36:20 +0300

    Add global get methods to BadgesController
    -Add get_earned_global method and its helper methods
    -Add get_unearned_global method and its helper methods

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,42 +1,81 @@
 class BadgesController < ApplicationController
+  
   # GET /user-badges/course/:course_id/earned
   def get_earned_in_course
-    course_id = params["course_id"].to_s
+    get_user_id
+    course_id = params["course_id"].to_i
     earned_badges = find_earned_in_course(course_id)
     render json: {"data" => earned_badges }
   end
+  
   # GET /user-badges/course/:course_id/unearned
   def get_unearned_in_course
-    course_id = params["course_id"].to_s
+    get_user_id
+    course_id = params["course_id"].to_i  
     earned_in_course = find_earned_in_course(course_id)
     all_badges_in_course = find_all_in_course(course_id)
     unearned_in_course = all_badges_in_course - earned_in_course
     render json: {"data" => unearned_in_course}
   end
-
+  
   # GET /user-badges/global/earned
   def get_earned_global
-    earned_badges = find_earned
+    get_user_id
+    earned_badges = find_all_earned
+    render json: {"data" => earned_badges}
   end
-
+  
   # GET /user-badges/global/unearned
   def get_unearned_global
+    get_user_id
+    all_badges = find_all_badges
+    earned_badges = find_all_earned
+    unearned = all_badges - earned_badges
+    render json: {"data" => unearned}
   end
-
+  
+  def get_user_id
+    @user_id = @token.user_id.to_s
+  end
+  
+  # Returns badges user has done in given course
   def find_earned_in_course(course_id)
     earned_in_course = []
     Badge.find_each do |badge|
-      earned_in_course << badge.badge_def if (badge.course_id == course_id)
+      course_is_right = badge.course_id == course_id
+      user_is_right = badge.user_id == @user_id
+      earned_in_course << badge.badge_def if (course_is_right && user_is_right)
     end
     earned_in_course
   end
-
+  
+  # Returns all possible badges in given course
   def find_all_in_course(course_id)
     all_badges_in_course = []
     BadgeDef.find_each do |bdef|
-      all_badges_in_course << bdef if (bdef.course_id == course_id)
+      all_badges_in_course << bdef if (course_id == bdef.course_id)
     end
     all_badges_in_course 
   end
-
+  
+  def find_all_earned
+    earned_badges = []
+    Badge.find_each do |badge|
+      earned_badges << badge.badge_def if (badge.user_id == @user_id)
+    end
+    earned_badges
+  end
+  
+  def find_all_badges
+    all_badges = []
+    BadgeDef.find_each do |bdef|
+      all_badges << bdef
+    end
+    all_badges
+  end
+  
+  def get_user_id
+    @user_id
+  end
+  
 end

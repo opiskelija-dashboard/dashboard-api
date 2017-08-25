0aef2de8a685b9b11c26464807aedd1b0c094bac
Juuha <juha.ritakoski@hotmail.com>
2017-08-25 13:02:14 +0300

    Rename column in badge_codes table
    -Rename column bugs to active, so it better reflects what it is for in badge_codes table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822092813) do
+ActiveRecord::Schema.define(version: 20170825092917) do
 
   create_table "badge_codes", force: :cascade do |t|
     t.string   "name",                               null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170822092813) do
     t.string   "code",                               null: false
     t.integer  "created_by"
     t.integer  "modified_by"
-    t.boolean  "bugs",               default: false
+    t.boolean  "active",             default: false
     t.boolean  "course_points_only", default: true
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false

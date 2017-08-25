84b10cb6f4650839bb4be8df252b53f32c065e24
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-25 13:11:06 +0300

    remove old badge end point

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,11 @@ Rails.application.routes.draw do
   get '/skill-mastery/course/:course_id/all', to: 'skill_masteries#all' 
   get '/skill-mastery/course/:course_id/combined', to: 'skill_masteries#combined'
   
-  
   get '/leaderboard/course/:course_id', to: 'leaderboards#get_range'
   get '/leaderboard/course/:course_id/all', to: 'leaderboards#get_all'
   get '/leaderboard/course/:course_id/whereis/current', to: 'leaderboards#find_current_user'
   get '/leaderboard/course/:course_id/whereis/:user_id', to: 'leaderboards#find_user'
   get '/leaderboard/course/:course_id/update', to: 'leaderboards#update_points'
-
-  get '/badges', to: 'badges#get_all_badges'
 
   get '/heatmap/courses/:course_id/all', to: 'heatmaps#everyones_points_average'
   get '/heatmap/courses/:course_id/current-user', to: 'heatmaps#current_users_points'

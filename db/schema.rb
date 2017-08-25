75b93b995c9fc2876e6a5847339209be8730b164
Juuha <juha.ritakoski@hotmail.com>
2017-08-25 15:08:02 +0300

    Remove course_id from Badges table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825092917) do
+ActiveRecord::Schema.define(version: 20170825120546) do
 
   create_table "badge_codes", force: :cascade do |t|
     t.string   "name",                               null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20170825092917) do
   create_table "badges", force: :cascade do |t|
     t.integer  "badge_def_id", null: false
     t.integer  "user_id",      null: false
-    t.integer  "course_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
